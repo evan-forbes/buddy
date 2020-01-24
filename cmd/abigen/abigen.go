de919eec0903e53f8cd8524567405b1e32dbf3c3
Evan Forbes <evan.samuel.forbes@gmail.com>
2020-01-23 22:39:16 -0600

    implemented cli

--- cmd/abigen/abigen.go
+++ cmd/abigen/abigen.go
@@ -3,86 +3,105 @@ package abigen
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/pkg/errors"
-
 	"github.com/evan-forbes/buddy/bind"
-	"github.com/evan-forbes/wand"
+	"github.com/pkg/errors"
+	cli "gopkg.in/urfave/cli.v1"
 )
-
-// TODOS:
-// - load files properly and check local path for many bins and abis
-// - generate code maybe modify to generate into files by type
-// - figure out how abigen inputs types
-// - write to file
-// - generate code and fix bugs
 
 // Cast runs the abigen command
 func Cast(ctx *cli.Context) error {
-	// set flags and args
-	flags := ctx.Flags()
-	args := ctx.Args()
-	// check to see if path was provided
-	path := args["abigen"]
-
-	if path != "" !strings.Contains(path, "--") {
-		// load all bins abi and types
+	////// check for flags //////
+	// fail if no package is specified
+	if ctx.String("pkg") == "" {
+		return errors.New("No package declared. Use flag --pkg or -p")
 	}
-
-	// use specific flag for paths OR look for files in the working directory
-	abiPath, has := flags["abi"]
-	if !has {
-		// search for abi file in current directory
-		abiPath, has = findFile(path, "abi")
+	// set value of type (default is whatever pkg is set to)
+	var tp string
+	if ctx.String("type") == "" {
+		tp = ctx.String("pkg")
+	}
+	// use defaults for abi and bin file locations (".")
+	abiPath := ctx.String("abi")
+	binPath := ctx.String("bin")
+	path := "."
+	if ctx.NArg() > 0 {
+		path = ctx.Args().Get(1)
+	}
+	// try to find paths to abi and bin files if not specified.
+	if abiPath == "." {
+		newABIPath, has := findFile(path, ".abi")
 		if !has {
-			fmt.Println("could not find abi file \n(use flag --abi= or ensure you have a abi in the working dir)")
-			return
+			return errors.Errorf("Could not find a .abi file in %s", path)
 		}
+		abiPath = newABIPath
 	}
-	binPath, has := flags["bin"]
-	if !has {
-		// search for bin file in current directory
-		binPath, _ = findFile(path, "bin")
+	if binPath == "." {
+		binPath, _ = findFile(path, ".bin")
 	}
-
-	// Load files
-	a, err := loadABI(abiPath)
+	// load the files
+	jsonABI, hexBin, err := openFiles(abiPath, binPath)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return errors.Wrapf(err, "Problem loading files in abi path: %s bin path: %s", abiPath, binPath)
 	}
-	hexBin := loadBin(binPath)
+	// generate bindings
+	code, err := bind.Bind(
+		[]string{tp},
+		[]string{jsonABI},
+		[]string{hexBin},
+		ctx.String("pkg"),
+	)
+	// write to file
+	filename := fmt.Sprintf("%s_gen.go", tp)
+	if ctx.String("out") != "" {
+		filename = ctx.String("out")
+	}
+	err = ioutil.WriteFile(filename, []byte(code), 0644)
+	if err != nil {
+		return err
+	}
 
-	bind.Bind()
-
+	return nil
 }
 
-func loadBin(path string) string {
+// open files
+func openFiles(abiPath, binPath string) (a, b string, err error) {
+	b, err = loadBin(binPath)
+	if err != nil {
+		return a, b, err
+	}
+	a, err = loadABI(abiPath)
+	if err != nil {
+		return a, b, err
+	}
+	return a, b, nil
+}
+
+// load the binary into a string, if path is not viable ignore
+func loadBin(path string) (string, error) {
 	if path == "" {
-		return ""
+		return "", nil
 	}
 	// load bin
 	rawBin, err := ioutil.ReadFile(path)
 	if err != nil {
-		fmt.Println("Could read bin file:", path, err)
-		return ""
+		return "", errors.Wrapf(err, "Could read bin file at path: %s", path)
 	}
-	return rawBin
+	return string(rawBin), nil
 }
 
-func loadABI(path string) (*abi.ABI, error) {
-	abiFile, err := os.Open(path)
-	if err != nil {
-		return nil, errors.Wrapf(err, "Could not open abi file: %s", path)
+// load abi file into a string, throw error if path not viable
+func loadABI(path string) (string, error) {
+	if path == "" {
+		return "", errors.New("could not load abi json")
 	}
-	contractABI, err := abi.JSON(abiFile)
+	// load abi
+	rawABI, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, errors.Wrapf(err, "could not parse abi file: %s", path)
+		return "", errors.Wrapf(err, "Could read abi file: %s", path)
 	}
-	return &contractABI, nil
+	return string(rawABI), nil
 }
 
 // findFile returns the first file found with the provided type
@@ -104,3 +123,38 @@ func findFile(path, ext string) (string, bool) {
 	}
 	return "", false
 }
+
+// Recycling bin
+// // set flags and args
+// flags := ctx.Flags()
+// args := ctx.Args()
+// // check to see if path was provided
+// path := args["abigen"]
+
+// if path != "" !strings.Contains(path, "--") {
+// 	// load all bins abi and types
+// }
+
+// // use specific flag for paths OR look for files in the working directory
+// abiPath, has := flags["abi"]
+// if !has {
+// 	// search for abi file in current directory
+// 	abiPath, has = findFile(path, "abi")
+// 	if !has {
+// 		fmt.Println("could not find abi file \n(use flag --abi= or ensure you have a abi in the working dir)")
+// 		return
+// 	}
+// }
+// binPath, has := flags["bin"]
+// if !has {
+// 	// search for bin file in current directory
+// 	binPath, _ = findFile(path, "bin")
+// }
+
+// // Load files
+// a, err := loadABI(abiPath)
+// if err != nil {
+// 	fmt.Println(err)
+// 	return
+// }
+// hexBin := loadBin(binPath)
--- main.go
+++ main.go
@@ -4,18 +4,66 @@ import (
 	"log"
 	"os"
 
+	cli "gopkg.in/urfave/cli.v1"
+
 	"github.com/evan-forbes/buddy/cmd/abigen"
-	"github.com/urfave/cli"
 )
+
+// Planned Commands:
+//  - mock
+//  - sim
+//  - abigen
 
 func main() {
 	app := cli.NewApp()
+	app.EnableBashCompletion = true
+	app.Name = "buddy"
 
-	app.Commands = []*cli.Command{
+	// var (
+	// 	abiPath string
+	// 	binPath string
+	// 	tp      string
+	// )
+
+	abiFlags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "abi, a",
+			Value: ".",
+			Usage: "path to abi (.json or .abi)",
+			// Destination: &abiPath,
+		},
+		cli.StringFlag{
+			Name:  "bin, b",
+			Value: ".",
+			Usage: "path to contract binary (usually a .bin)",
+			// Destination: &binPath,
+		},
+		cli.StringFlag{
+			Name:  "type, t",
+			Value: "",
+			Usage: "specify the main type",
+			// Destination: &tp,
+		},
+		cli.StringFlag{
+			Name:  "pkg, p",
+			Value: "",
+			Usage: "specify the package name",
+			// Destination: &tp,
+		},
+		cli.StringFlag{
+			Name:  "out, o",
+			Value: "",
+			Usage: "specify the output file name (default = type_gen.go",
+			// Destination: &tp,
+		},
+	}
+
+	app.Commands = []cli.Command{
 		{
 			Name:   "abigen",
 			Usage:  "generate interface and mock friendly go bindings",
 			Action: abigen.Cast,
+			Flags:  abiFlags,
 		},
 	}
 
