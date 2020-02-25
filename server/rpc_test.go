a66a1f4bcbc790cc44df00227d49f84bb39633e9
Evan Forbes <evan.samuel.forbes@gmail.com>
2020-02-25 14:31:05 -0600

    marked for deletion

--- server/rpc_test.go
+++ server/rpc_test.go
@@ -1,14 +1,8 @@
 package server
 
 import (
-	"testing"
-	"sync"
-	"context"
-
 	"github.com/ethereum/go-ethereum/core"
-	"github.com/ethereum/go-ethereum/node"
 	"github.com/evan-forbes/buddy/sim"
-	"github.com/evan-forbes/buddy/cmd"
 )
 
 func defaultGenesisAlloc() core.GenesisAlloc {
@@ -16,17 +10,17 @@ func defaultGenesisAlloc() core.GenesisAlloc {
 	return accnts.Genesis()
 }
 
-func TestServer(t *testing.T) {
-	var wg sync.WaitGroup
-	mngr := cmd.NewManager(context.Background(), &wg)
+// func TestServer(t *testing.T) {
+// 	var wg sync.WaitGroup
+// 	mngr := cmd.NewManager(context.Background(), &wg)
 
-	go mngr.Listen()
-	Boot(&node.Config{
-		HTTPHost: "127.0.0.1",
-		HTTPPort: 8537,
-		WSHost: "127.0.0.1",
-		WSPort: 8538,
-	},)
+// 	go mngr.Listen()
+// 	Boot(&node.Config{
+// 		HTTPHost: "127.0.0.1",
+// 		HTTPPort: 8537,
+// 		WSHost: "127.0.0.1",
+// 		WSPort: 8538,
+// 	},)
 
-	<-mngr.Done()
-}
+// 	<-mngr.Done()
+// }
--- server/server.go
+++ server/server.go
@@ -1,103 +1,89 @@
 package server
 
-import (
-	"log"
-	"time"
-
-	"github.com/ethereum/go-ethereum/eth/filters"
-	"github.com/ethereum/go-ethereum/node"
-	"github.com/ethereum/go-ethereum/p2p"
-	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/evan-forbes/buddy/ethapi"
-	"github.com/evan-forbes/buddy/sim"
-	"github.com/evan-forbes/buddy/thereum"
-	"github.com/pkg/errors"
-)
-
 // I need to plug the simulated backend interface into the interfaces that it
 // implements, and then plug those things into the node rpc example below
 
 // todo: find out all apis included in eth/backend.go ethereum.APIs() and implement/test one by connected it below
 // 		 - make a client that connects to the local host or whatever ip is being used.
 
-type Server struct {
-	Backend *thereum.Thereum
-}
+// type Server struct {
+// 	Backend *thereum.Thereum
+// }
 
-func (s *Server) Protocols() []p2p.Protocol { return nil }
+// func (s *Server) Protocols() []p2p.Protocol { return nil }
 
-func (s *Server) APIs() []rpc.API {
-	// nonceLock := new(AddrLocker)
-	return []rpc.API{
-		{
-			Namespace: "eth",
-			Version:   "1.0",
-			Service:   filters.NewPublicFilterAPI(s.Backend, false),
-			Public:    true,
-		},
-		{
-			Namespace: "eth",
-			Version:   "1.0",
-			Service:   ethapi.NewPublicEthereumAPI(s.Backend),
-			Public:    true,
-		},
-		{
-			Namespace: "eth",
-			Version:   "1.0",
-			Service:   ethapi.NewPublicBlockChainAPI(s.Backend),
-			Public:    true,
-		}, // might need to implement transaction pool for the simback
-		// {
-		// 	Namespace: "eth",
-		// 	Version:   "1.0",
-		// 	Service:   NewPublicTransactionPoolAPI(s.Backend, nonceLock),
-		// 	Public:    true,
-		// },
-		// {
-		// 	Namespace: "txpool",
-		// 	Version:   "1.0",
-		// 	Service:   NewPublicTxPoolAPI(s.Backend),
-		// 	Public:    true,
-		// },
-	}
-}
+// func (s *Server) APIs() []rpc.API {
+// 	nonceLock := new(AddrLocker)
+// 	return []rpc.API{
+// 		{
+// 			Namespace: "eth",
+// 			Version:   "1.0",
+// 			Service:   filters.NewPublicFilterAPI(s.Backend, false),
+// 			Public:    true,
+// 		},
+// 		{
+// 			Namespace: "eth",
+// 			Version:   "1.0",
+// 			Service:   ethapi.NewPublicEthereumAPI(s.Backend),
+// 			Public:    true,
+// 		},
+// 		{
+// 			Namespace: "eth",
+// 			Version:   "1.0",
+// 			Service:   ethapi.NewPublicBlockChainAPI(s.Backend),
+// 			Public:    true,
+// 		},
+// 		{
+// 			Namespace: "eth",
+// 			Version:   "1.0",
+// 			Service:   NewPublicTransactionPoolAPI(s.Backend, nonceLock),
+// 			Public:    true,
+// 		},
+// 		{
+// 			Namespace: "txpool",
+// 			Version:   "1.0",
+// 			Service:   NewPublicTxPoolAPI(s.Backend),
+// 			Public:    true,
+// 		},
+// 	}
+// }
 
-func (s *Server) Start(p *p2p.Server) error {
-	// Don't want to start any p2p servers, the rest is handled by the node
-	return nil
-}
+// func (s *Server) Start(p *p2p.Server) error {
+// 	// Don't want to start any p2p servers, the rest is handled by the node
+// 	return nil
+// }
 
-func (s *Server) Stop() error {
-	s.Backend.Close()
-	s.Backend.ChainDb().Close()
-	return nil
-}
+// func (s *Server) Stop() error {
+// 	s.Backend.Close()
+// 	s.Backend.ChainDb().Close()
+// 	return nil
+// }
 
-func Constructor(ctx *node.ServiceContext) (node.Service, error) {
-	accnts := sim.NewAccounts("owner", "Alice", "Bob", "Celine", "Doug", "Erin", "Frank")
-	genesisAlloc := accnts.Genesis()
-	s := &Server{
-		Backend: sim.NewSimulatedBackend(genesisAlloc, uint64(4712388)),
-	}
-	return s, nil
-}
+// func Constructor(ctx *node.ServiceContext) (node.Service, error) {
+// 	accnts := sim.NewAccounts("owner", "Alice", "Bob", "Celine", "Doug", "Erin", "Frank")
+// 	genesisAlloc := accnts.Genesis()
+// 	s := &Server{
+// 		Backend: sim.NewSimulatedBackend(genesisAlloc, uint64(4712388)),
+// 	}
+// 	return s, nil
+// }
 
-func Boot(config *node.Config) error {
-	stack, err := node.New(config)
-	if err != nil {
-		return errors.Wrapf(err, "Could not boot server %s", config.Name)
-	}
-	defer stack.Close()
-	if err = stack.Register(Constructor); err != nil {
-		log.Fatalf("Failed to register service: %v", err)
-	}
-	// Boot up the entire protocol stack, do a restart and terminate
-	if err = stack.Start(); err != nil {
-		log.Fatalf("Failed to start the protocol stack: %v", err)
-	}
-	time.Sleep(time.Second * 8)
-	if err = stack.Stop(); err != nil {
-		log.Fatalf("Failed to stop the protocol stack: %v", err)
-	}
-	return nil
-}
+// func Boot(config *node.Config) error {
+// 	stack, err := node.New(config)
+// 	if err != nil {
+// 		return errors.Wrapf(err, "Could not boot server %s", config.Name)
+// 	}
+// 	defer stack.Close()
+// 	if err = stack.Register(Constructor); err != nil {
+// 		log.Fatalf("Failed to register service: %v", err)
+// 	}
+// 	// Boot up the entire protocol stack, do a restart and terminate
+// 	if err = stack.Start(); err != nil {
+// 		log.Fatalf("Failed to start the protocol stack: %v", err)
+// 	}
+// 	time.Sleep(time.Second * 8)
+// 	if err = stack.Stop(); err != nil {
+// 		log.Fatalf("Failed to stop the protocol stack: %v", err)
+// 	}
+// 	return nil
+// }
