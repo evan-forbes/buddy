ce03fb18eba0e601cb77358ac41bc6573b0aa90b
Evan Forbes <evan.samuel.forbes@gmail.com>
2020-03-19 21:36:04 -0500

    added default node config

--- cmd/boot/boot.go
+++ cmd/boot/boot.go
@@ -8,11 +8,23 @@ import (
 )
 
 func Boot(c *cli.Context) error {
+
 	// Create a network node to run protocols with the default values.
-	stack, err := node.New(&node.Config{})
+	stack, err := node.New(&node.DefaultConfig)
 	if err != nil {
 		log.Fatalf("Failed to create network node: %v", err)
 	}
 	defer stack.Close()
 	return nil
+}
+
+var defNodeconf = &node.Config{
+	Name:      "Thereum",
+	UserIdent: "Founder",
+	Version:   "0.0.1",
+	DataDir:   ".",
+	HTTPHost:  "127.0.0.1",
+	HTTPPort:  420024,
+	WSHost:    "127.0.0.1",
+	WSPort:    420025,
 }
